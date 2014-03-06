e4bf0a05be00f364d68054871a2c0a167d527a33
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-03-06 17:53:33 -0500

    integrating with epitopes

--- immunogenicity.py
+++ immunogenicity.py
@@ -14,15 +14,15 @@ class ImmunogenicityRFModel(PipelineElement):
                  # 'drop' | 'keep' | 'positive' | 'negative'
                  human = True,
                  hla_type = 1,
-                 max_ngram = 1, 
+                 ngram = 1, 
                  reduced_alphabet = None):
     self.name = name
-    self._X, self._Y, self._feature_transformer = iedb.load_tcell(
+    self._X, self._Y, self._feature_transformer = iedb.load_tcell_dataset(
         assay_group=assay_group,
         noisy_labels = noisy_labels, 
         human = human, 
         mhc_class = mhc_class,
-        max_ngram = max_ngram, 
+        ngram = ngram, 
         reduced_alphabet = reduced_alphabet)
     self._model = ensemble.RandomForestClassifier(n_trees)
     self._model.fit(self._X, self._Y)
