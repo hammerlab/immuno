727661e88e51db2b80e98cd280073c76a291273a
arahuja <aahuja11@gmail.com>
2014-04-08 11:37:26 -0400

    removing unused script files

--- scripts/annotate_with_snpeff.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Xmx4g -jar snpEff.jar eff -v GRCh37.66 ${1} > ${1}.eff.vcf
--- scripts/setup_snpeff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-wget http://sourceforge.net/projects/snpeff/files/snpEff_v3_0_core.zip
-unzip snpEff_v3_0_core.zip
-mv snpEff_3_0 snpEff 
-cd snpEff
-java -jar snpEff.jar download GRCh37.66
