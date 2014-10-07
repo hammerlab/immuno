5fe389b3a9b231f08fbaa29fa93d2906587fab8b
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-10-07 16:25:02 -0400

    fixed whitespace in read_hla_alleles test

--- test/test_read_hla_alleles.py
+++ test/test_read_hla_alleles.py
@@ -3,16 +3,14 @@ from immuno.hla_file import read_hla_file
 
 def test_read_hla():
     # contains two C identical C alleles
-	hla_filename = 'data/SKCM.hla'
-	alleles = read_hla_file(hla_filename)
-	print alleles
-	assert alleles == \
-		[
-		  'HLA-A*01:01',
-		  'HLA-A*03:01',
-		  'HLA-B*08:01',
-		  'HLA-B*07:02',
-		  'HLA-C*07:02',
-		  'HLA-C*07:02'
-		]
-	assert len(set(alleles)) == 5
+    hla_filename = 'data/SKCM.hla'
+    alleles = read_hla_file(hla_filename)
+    print alleles
+    assert alleles == [
+        'HLA-A*01:01',
+        'HLA-A*03:01',
+        'HLA-B*08:01',
+        'HLA-B*07:02',
+        'HLA-C*07:02',
+	'HLA-C*07:02'
+   ]
