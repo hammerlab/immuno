126328cbed6a9c4dbcf04c6c6e4aa2aa0213c45d
Tavi Nathanson <tavi.nathanson@gmail.com>
2014-10-15 10:04:15 -0400

    Adding a very basic file upload form, which sends a file to /uploads

--- immuno/ui.py
+++ immuno/ui.py
@@ -2,13 +2,16 @@ from common import str2bool
 from vcf import load_vcf
 
 from flask import Flask
-from flask import redirect, render_template, url_for
+from flask import redirect, request, render_template, url_for, send_from_directory
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.user import (current_user, login_required, UserManager,
     UserMixin, SQLAlchemyAdapter)
 from flask_mail import Mail, Message
+from werkzeug import secure_filename
 from os import environ, getcwd
 from os.path import exists, join
+
+ALLOWED_EXTENSIONS = set(['vcf', 'hla'])
 
 class ConfigClass(object):
     # Custom config
@@ -17,6 +20,7 @@ class ConfigClass(object):
     USE_RELOADER = str2bool(environ.get('IMMUNO_USE_RELOADER', False))
     VARIANT_PATH = environ.get('IMMUNO_VARIANT_PATH', getcwd())
     HLA_PATH = environ.get('IMMUNO_HLA_PATH', getcwd())
+    UPLOAD_FOLDER = join(getcwd(), 'uploads')
 
     # Flask config
     SECRET_KEY = environ.get('IMMUNO_SECRET_KEY')
@@ -33,11 +37,11 @@ class ConfigClass(object):
     USER_ENABLE_REGISTRATION = True
     USER_ENABLE_RETYPE_PASSWORD = True
     USER_ENABLE_USERNAME = False
-    USER_CONFIRM_EMAIL_EXPIRATION = 2*24*3600
+    USER_CONFIRM_EMAIL_EXPIRATION = 2 * 24 * 3600
     USER_PASSWORD_HASH = 'bcrypt'
     USER_PASSWORD_HASH_MODE = 'passlib'
     USER_REQUIRE_INVITATION = False
-    USER_RESET_PASSWORD_EXPIRATION = 2*24*3600
+    USER_RESET_PASSWORD_EXPIRATION = 2 * 24 * 3600
     USER_SEND_PASSWORD_CHANGED_EMAIL = True
     USER_SEND_REGISTERED_EMAIL = True
     USER_SEND_USERNAME_CHANGED_EMAIL = False
@@ -94,3 +98,22 @@ def patient(patient_filename):
         patient_id = variant_file_path,
         variant_filename = variant_file_path,
         vcf = vcf_rows)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+@login_required
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+@login_required
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
