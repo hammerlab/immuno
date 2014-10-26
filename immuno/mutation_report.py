9625965a6ccf5ed518cf0e177ccf784646868b3c
Tavi Nathanson <tavi.nathanson@gmail.com>
2014-10-26 09:05:57 -0400

    Removing description from mutation report

--- immuno/mutation_report.py
+++ immuno/mutation_report.py
@@ -195,10 +195,6 @@ def group_epitopes(scored_epitopes):
         peptide_entry["PeptideMutationInfo"] = head.PeptideMutationInfo
         peptide_entry["GeneInfo"] = head.GeneInfo
         peptide_entry['Gene'] = head.Gene
-        peptide_entry['Description'] = "%s %s %s %s %s (%s) : %s" % (
-                head.chr, head.pos, head.ref, head.alt, head.Gene,
-                head.TranscriptId, head.PeptideMutationInfo
-        )
         peptide_entry['Epitopes'] = []
         for (epitope, epitope_start, epitope_end), epitope_group in \
                 transcript_group.groupby(
